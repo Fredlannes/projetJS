b58e0f7910c037d037bae6b2e8b7f5d8313bf799
Frederic Lannes <lannes.frederic@orange.fr>
2023-06-07 19:10:03 +0200

    add some events

--- projet1/script.js
+++ projet1/script.js
@@ -2,25 +2,47 @@
 function RefrechWysiwyg(){
     let textTitre = document.getElementById("titreWysiwyg").value;
     document.querySelector("#titreWysiwygResultat").textContent = textTitre
+
+    let colorTitle = document.getElementById("colorTitleWysiwyg").value;
+    document.querySelector("#titreWysiwygResultat").style.color = colorTitle
+    
+    let sizeTitle = document.getElementById("sizeTitleWysiwyg").value;
+    document.querySelector("#titreWysiwygResultat").style.fontSize = `${sizeTitle}px`
     
     let contentText = document.getElementById("contentWysiwyg").value;
     document.querySelector("#contentWysiwygResultat").textContent = contentText
     
     let imgTitre = document.getElementById("imgWysiwyg").value;
     document.getElementById("imgWysiwygResultat").src = imgTitre
+
+    let imgMaxWidth = document.getElementById("imgMaxWidthWysiwyg").value;
+    document.getElementById("imgWysiwygResultat").style.width = ''+imgMaxWidth+'px'
+
 }
 
-let btn = document.getElementById("btnGenererWysiwyg");
+function transformFieldsetIn(){
+    let tousImput = document.querySelectorAll("input");
+    tousImput.forEach(myInput => {
+        myInput.style.padding = '5px'
+    })
+}
+
+function transformFieldsetOut(){
+    let tousImput = document.querySelectorAll("input");
+    tousImput.forEach(myInput => {
+        myInput.style.padding = 'inherit'
+    })
+}
+
+const btn = document.getElementById("btnGenererWysiwyg");
 btn.addEventListener('click', RefrechWysiwyg);
+const mesInputsWithEvent = document.querySelectorAll(".onChangeRefrechWysiwyg");
 
-// let inputTitre = document.querySelector("#titreWysiwyg");
-// inputTitre.addEventListener('keyup', RefrechWysiwyg)
-// inputTitre.addEventListener('change', RefrechWysiwyg)
-
-let mesInputsWithEvent = document.querySelectorAll(".onChangeRefrechWysiwyg");
-
-mesInputsWithEvent.forEach(elements => {
-    elements.addEventListener('keyup', RefrechWysiwyg);
-    elements.addEventListener('change', RefrechWysiwyg);
+mesInputsWithEvent.forEach(monImput => {
+    monImput.addEventListener('keyup', RefrechWysiwyg);
+    monImput.addEventListener('change', RefrechWysiwyg);
+    
+    monImput.addEventListener('mouseover', transformFieldsetIn);
+    monImput.addEventListener('mouseleave', transformFieldsetOut);
 
 })
