9870d649472bb633cb93c73e9208b5afebc672e7
Frederic Lannes <lannes.frederic@orange.fr>
2023-12-27 14:54:53 +0100

    cour ajax 2

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -1,15 +1,23 @@
-let btn = document.getElementById("afficherUser")
-btn.addEventListener("click", () => {
-    showUsers()
-})
+// let btn = document.getElementById("afficherUser")
+// btn.addEventListener("click", () => {
+//     showUsers()
+// })
 
-function showUsers() {
+//on recupere le numero de page et on continue
+// function showUsers() {
+//     const nbPage = document.getElementById("numberPageUser").value;
+//     getUsers(nbPage)
+    
+// }
+const loader = '<div class="lds-ripple"><div></div><div></div></div>'
+//on fait l'appel ajax et on continue
 
-    //initialise ma requete
-    // je mets ma lettre dans mon enveloppe
-
+function getUsers(numeroPage){
+    document.getElementById("allUtilisateurs").innerHTML = loader
+    document.getElementById("pagination").innerHTML = ""
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', "https://reqres.in/api/users?page=1");
+    const url = "https://reqres.in/api/users?delay=0.5&page=" + numeroPage;
+    xhr.open('GET', url);
 
     //je veux attrapper le retour de ma requette
     xhr.addEventListener('readystatechange', () => {
@@ -18,12 +26,7 @@ function showUsers() {
                 //on gere le retour de notre appel ajax
                 //console.log("Response = " + xhr.response);
                 const object = JSON.parse(xhr.response);
-
-                let myhtml = "";
-                object.data.forEach(element => {
-                    myhtml += "<div><p>"+ element.first_name +" "+ element.last_name +" </p></div>";
-                });
-                document.getElementById("allUtilisateurs").innerHTML = myhtml;
+                setUsersInPage(object)
             }
             else if(xhr.status == 404){
                 alert("impossible de trouver l'url de la page")
@@ -36,3 +39,33 @@ function showUsers() {
     xhr.send();
 }
 
+ //on affiche le resultat de l'appel ajax dans la page
+function setUsersInPage(listUsers){
+    // on ajoute la liste des utilisateurs
+    let myhtml = "";
+    listUsers.data.forEach(element => {
+        myhtml += '<div><img src="'+element.avatar+'"/><p>'+ element.first_name +' '+ element.last_name +' </p></div>';
+    });
+    document.getElementById("allUtilisateurs").innerHTML = myhtml;
+
+    //on crée la pagination
+    let nbPage = listUsers.total_pages;
+    let currentPage = listUsers.page;
+
+    let htmlPagination = ""
+    for(let i=1; i<=nbPage; i++){
+        if(i == currentPage){
+            htmlPagination += '<button class="btn active" disabled> '+i+' </button>'
+            
+        }else{
+            htmlPagination += '<button class="btn" onclick="getUsers('+i+')"> '+i+' </button>'
+
+        }
+    }
+
+    document.getElementById("pagination").innerHTML = htmlPagination;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    getUsers(1)
+})
